09068c997bef0eccec30d6807c8f854c81d82ed1
ZahraIbr <ZahraIbr@Ibrahim>
2021-07-05 10:14:26 +0300

    User authentication and registration implemention ,integrated with mysqlDB on rds

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken')
+const {getUser, insertUser} = require("../db/mysql-repo");
+const bcrypt = require('bcrypt')
+
+
 /**
  *
  * @param req
@@ -5,7 +10,34 @@
  * @returns {Promise<void>}
  */
 const authenticate = async (req, res) => {
+    const {password, email} = req.body
+    if (!password || !email) {
+        res.sendStatus(400)
+        return
+    }
+    //Get user from DB
+    getUser(req.body.email, async function (result) {
 
+        if (result.length == 0) {
+            res.status(403).send("this user does not exist")
+            return;
+        }
+        // // Compare the password with the password in the database
+        const valid = await bcrypt.compare(req.body.password, result[0].password)
+        if (!valid) {
+            res.status(403).send("Invalid email or password")
+            return;        }
+        //
+        const token = jwt.sign({
+            id: result[0].id,
+            user: result[0].email,
+        }, process.env.JWT_PRIVATE_SECRET, {expiresIn: "15m"});
+
+        res.send({
+            ok: true,
+            token: token
+        });
+    })
 }
 /**
  *
@@ -14,8 +46,21 @@ const authenticate = async (req, res) => {
  * @returns {Promise<void>}
  */
 const register = async (req, res) => {
+    const user = {email: req.body.email, password: await passwordEncoder(req.body.password)}
+    await insertUser(user, function (results) {
+        res.status(200).send(results)
+    });
 }
 
+/**
+ *
+ * @param password
+ * @returns {Promise<*>}
+ */
+async function passwordEncoder(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
 
 module.exports = {
     authenticate, register
